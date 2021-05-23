3a4c93978e1cd4efd8fea9c3a4d782a7b8db985a
Cameron Young <Cameronslatton@gmail.com>
2021-05-22 22:26:07 -0500

    Set up test
    Set up a test for the FizzBuzz method. Step 1.

---
+++ FizzBuzz_Kata/Kata.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FizzBuzz_Kata
+{
+    public class Kata
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
