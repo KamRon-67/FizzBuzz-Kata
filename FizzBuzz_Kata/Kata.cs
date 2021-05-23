f152afffd4a796e94d3424baf79d65a2d4633565
Cameron Young <Cameronslatton@gmail.com>
2021-05-22 23:10:01 -0500

    Added method and updated test
    This passes step one.

--- FizzBuzz_Kata/Kata.cs
+++ FizzBuzz_Kata/Kata.cs
@@ -4,6 +4,18 @@ namespace FizzBuzz_Kata
 {
     public class Kata
     {
+        public string FizzBuzz()
+        {
+            var listData = "";
+
+            for (int i = 0; i <= 100; i++)
+            {
+                listData += $"{i}" + "\n";
+            }
+
+            return listData;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
